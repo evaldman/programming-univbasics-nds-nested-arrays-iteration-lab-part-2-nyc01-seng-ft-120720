e74ffe527a24842cb5e60184f4058206a6373eef
charming-invocation-5395 <evaldman@yahoo.com>
2020-11-17 22:14:06 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ row_index = 0
 while row_index < src.count do 
   element_index = 0 
   lowest_value = 10000000
-  while element_index < src[row_index.].count do
+  while element_index < src[row_index].count do
     if src[row_index][element_index] < lowest_value
       lowest_value = src[row_index][element_index]
     end
